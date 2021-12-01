f390bf69088343ccc313765e10b5307c7a8ec91d
scollo26 <giovanniscollo26@gmail.com>
2021-12-01 17:41:04 +0100

    esercizio-con funzione

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+
+
+
 let containerCell = document.querySelector('.container-cell');
 let button = document.querySelector('.controls  button');
 const select = document.getElementById('level');
 console.log(containerCell);
 console.log(button);
 
-// const row = 10;
-// const col = 10;
-// let numberSquare = row * col;
-// let maxNumber = numberSquare;
+const row = 10;
+const col = 10;
+let numberSquare = row * col;
+let maxNumber = numberSquare;
 
 
 function arrayNumber(min, max) {
@@ -21,67 +24,110 @@ button.addEventListener('click', function() {
 
     const select = document.getElementById('level');
 
-// easy
+    // easy
 
-if( select.value == "easy"){
-    containerCell.innerHTML ='';
+    if( select.value == "easy"){
+        containerCell.innerHTML ='';
 
-    for (let i = 1; i < 101; i++) {
-        const square = document.createElement('div');
-        square.classList.add('easy');
-        square.classList.add('active');
-        square.append(i);
-        containerCell.append(square);
+        for (let i = 1; i <= 100; i++) {
+            const square = document.createElement('div');
+            square.classList.add('easy');
+            square.classList.add('active');
+            square.append(i);
+            containerCell.append(square);
 
-        square.addEventListener('click', function(){
-            square.classList.add('blue');
-        });
+            // click-cambia-colore-blu
+            square.addEventListener('click', function(){
+                square.classList.add('blue');
+            });
         
-    }
+        }
     // medium
-}else if( select.value == "medium"){
-    containerCell.innerHTML ='';
+    }else if( select.value == "medium"){
+        containerCell.innerHTML ='';
     
-    for (let i = 1; i < 81; i++) {
-        const square = document.createElement('div');
-        square.classList.add('medium');
-        square.classList.add('active');
-        
-        square.append(i);
-        containerCell.append(square);
+        for (let i = 1; i <= 81; i++) {
+            const square = document.createElement('div');
+            square.classList.add('medium');
+            square.classList.add('active');
+            square.append(i);
+            containerCell.append(square);
 
-        square.addEventListener('click', function(){
-            square.classList.add('blue');
-        });
+            // click-cambia-colore-blu
+            square.addEventListener('click', function(){
+                square.classList.add('blue');
+            });
         
         
-    }
+        }
 
     // hard
 
-}else{
-    containerCell.innerHTML ='';
-    for (let i = 1; i < 50; i++) {
-        const square = document.createElement('div');
-        square.classList.add('hard');
-        square.classList.add('active');
-        square.append(i);
-        containerCell.append(square);
+    }else{
+        containerCell.innerHTML ='';
+        for (let i = 1; i <= 49; i++) {
+            const square = document.createElement('div');
+            square.classList.add('hard');
+            square.classList.add('active');
+            square.append(i);
+            containerCell.append(square);
 
-
-        square.addEventListener('click', function(){
-            square.classList.add('blue');
-        });
+            // click-cambia-colore-blu
+            square.addEventListener('click', function(){
+                square.classList.add('blue');
+            });
         
             
+        }
+    
     }
     
-}
-    
+});
 
-    
-})
 
+// Esercizio-con-funzione
+
+// let button = document.querySelector('.controls  button');
+
+// button.addEventListener('click', function() {
+//     let numberEasy = 100;
+//     let numberMedium = 81;
+//     let numberHard = 49;
+//     let easy = "easy";
+//     let medium = "medium";
+//     let hard= "hard";
+//     const select = document.getElementById('level');
+
+//     function addDiv(number, level){
+//         let containerCell = document.querySelector('.container-cell');
+        
+        
+//         containerCell.innerHTML ='';
+
+//         for (let i = 1; i <= number; i++) {
+//             const square = document.createElement('div');
+//             square.classList.add(level,'active');
+//             square.append(i);
+//             containerCell.append(square);
+
+//             // click-cambia-colore-blu
+//             square.addEventListener('click', function(){
+//                 square.classList.add('blue');
+//             });
+    
+//         }
+
+//     }
+
+//     if( select.value == "easy"){
+//         addDiv(numberEasy,easy);
+//     }else if( select.value == "medium"){
+//         addDiv(numberMedium,medium);
+//     }else{
+//         addDiv(numberHard,hard);
+//     }
+
+// });
 
 
 
