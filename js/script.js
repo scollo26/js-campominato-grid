dba8464179b8a4196a07e6702028200effa9514f
scollo26 <giovanniscollo26@gmail.com>
2021-12-01 16:48:23 +0100

    click cambia colore-blu

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const select = document.getElementById('level');
 console.log(containerCell);
 console.log(button);
 
-const row = 10;
-const col = 10;
-let numberSquare = row * col;
-let maxNumber = numberSquare;
+// const row = 10;
+// const col = 10;
+// let numberSquare = row * col;
+// let maxNumber = numberSquare;
 
 
 function arrayNumber(min, max) {
@@ -15,44 +15,64 @@ function arrayNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// button
 
 button.addEventListener('click', function() {
 
     const select = document.getElementById('level');
+
+// easy
 
 if( select.value == "easy"){
     containerCell.innerHTML ='';
 
     for (let i = 1; i < 101; i++) {
         const square = document.createElement('div');
-        square.classList.add('cell');
+        square.classList.add('easy');
         square.classList.add('active');
         square.append(i);
         containerCell.append(square);
+
+        square.addEventListener('click', function(){
+            square.classList.add('blue');
+        });
         
     }
-
+    // medium
 }else if( select.value == "medium"){
     containerCell.innerHTML ='';
+    
     for (let i = 1; i < 81; i++) {
         const square = document.createElement('div');
-        square.classList.add('cell');
+        square.classList.add('medium');
         square.classList.add('active');
+        
         square.append(i);
         containerCell.append(square);
+
+        square.addEventListener('click', function(){
+            square.classList.add('blue');
+        });
+        
         
     }
 
-    
+    // hard
 
-}else if( select.value == "hard"){
+}else{
     containerCell.innerHTML ='';
-    for (let i = 1; i < 51; i++) {
+    for (let i = 1; i < 50; i++) {
         const square = document.createElement('div');
-        square.classList.add('cell');
+        square.classList.add('hard');
         square.classList.add('active');
         square.append(i);
         containerCell.append(square);
+
+
+        square.addEventListener('click', function(){
+            square.classList.add('blue');
+        });
+        
             
     }
     
